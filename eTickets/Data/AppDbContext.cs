7a3b05bd006fbb0b091afa4e951610d686d4d4ba
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-09-26 11:54:07 +0400

    Added ShoppingCartItem model.

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -40,5 +40,6 @@ namespace eTickets.Data
         //Order relaited table
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
