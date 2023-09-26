18dbb78669331d4f285baa05b4516bdd33b63efd
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-09-26 12:05:58 +0400

    Set up ShoppingCart class.

---
+++ eTickets/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public AppDbContext _context { get; set; }
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public List<ShoppingCartItem> GetShoppingCartItems() 
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems
+                .Where(n => n.ShoppingCartId == ShoppingCartId)
+                .Include(n => n.Movie)
+                .ToList());
+        }
+
+        public double GetShoppingCardTotal()
+        {
+            return _context.ShoppingCartItems
+                .Where(n => n.ShoppingCartId == ShoppingCartId)
+                .Select(n => n.Movie.Price * n.Amount).Sum();
+        }
+    }
+}
