c153334eb802896f08318369df3ab8678fcbc4b5
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-09-26 13:07:28 +0400

    Added OrdersController file

---
+++ eTickets/Models/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿using eTickets.Data.Cart;
+
+namespace eTickets.Models.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCardTotal { get; set; }
+    }
+}
